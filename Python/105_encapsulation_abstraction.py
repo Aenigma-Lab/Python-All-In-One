ef33a6ab05c86f4a81981d60bf5ca3a910be6e07
Aenigma-Lab <f10.shubham@gmail.com>
2024-11-22 17:35:58 +0530

    encapsulation and abstraction.

---
+++ Python/105_encapsulation_abstraction.py
@@ -0,0 +1,36 @@
+# In this series we will talk about 
+# Encapsulation
+# Abstraction
+# Some special naming convention
+# Naming Mangling
+# python uses tim short method. s
+
+# __name() # this is not a convention
+
+class Phone:
+    def __init__(self, brand, model_name, price):
+        self.brand = brand
+        self.__model_name = model_name
+        self._price = price # accordin to naming convetion (_price) it consider as private but noting is private in python
+
+    def make_a_call(self, phone_number):
+        print(f"calling {phone_number}....")
+
+    def full_name(self):
+        return f"{self.brand} {self.model_name}"
+    
+# _name # convention of private name 
+# __name__ # it is called (dunder) or magic method.
+
+
+
+phone_1 = Phone('nokia','1100',1000)
+print(phone_1._price)
+phone_1._price = -1000 #if i change the price of phone
+print(phone_1._price)
+
+print(phone_1.__dict__) #{'brand': 'nokia', '_Phone__model_name': '1100', '_price': -1000}
+
+#------>>>>>   '_Phone__model_name' # this is not private 
+phone_1._PHone_modle_name = 'samsung'
+print(phone_1._Phone__model_name)
