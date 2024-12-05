801bcb107a479086d38775b2750cbac810630130
precihole <erpadmin@preciholesports.com>
2024-12-05 10:20:24 +0530

    Intoduction of built in errors.

---
+++ Python/111_Introduction_of_built_in_errors.py
@@ -0,0 +1,54 @@
+# built in errors
+# exception, how to handle them
+# raise you own error , dbugging etc.
+
+
+
+
+# Synta error-------------------------> 
+
+# def func:
+#   pass
+
+# name = 'shubham'*
+
+# indentation erros----------------------------->
+
+
+
+# def func():
+#     print('hello')
+#   print('world')
+
+
+
+# name error------------------------------->
+#  print(name)
+# print(func)
+
+
+
+# type error --------------------------------->
+# here number and string can not be added 
+
+# print(1+'shubham')           # here we can not use '+' but we can use *
+# print(6*'shubham')             # here we can not use '+' but we can use *
+
+
+
+#value error---------------------------------------->
+# if value in string like '5' and want to convert in int so we use int(s) but 
+# if the s = 'shubha' and then i convert to int it will give value error.
+# s = 'shubham'
+# print(int(s))
+
+# attribute error------------------------------------->
+# l = [1,2,3]
+# l.push('12')
+
+
+
+# key error------------------------------------------>
+d = {'name':'harshit'}
+print(d)
+print(d['surname'])
