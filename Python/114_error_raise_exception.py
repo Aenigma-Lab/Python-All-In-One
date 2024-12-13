9bb99c616c8443333654c0bdea757101a68fb4ed
precihole <erpadmin@preciholesports.com>
2024-12-13 14:28:46 +0530

    updated on error raise_ exception.

--- Python/114_error_raise_exception.py
+++ Python/114_error_raise_exception.py
@@ -8,14 +8,19 @@ class MobileStore:
         self.mobiles = []
 
     def add_mobile(self, new_mobile):
-        if isinstance(new_mobile, Mobile):
+        # if isinstance(new_mobile, Mobile):
             self.mobiles.append(new_mobile)
-        else:
-            raise TypeError('new Mobile should be an object of the Mobile class.')
+        # else:
+        #     raise TypeError('new Mobile should be an object of the Mobile class.')
 
 # Create instances and test
-samsung = Mobile('One Plus 10')
+oneplus = Mobile('One Plus 10')
 mobile_store = MobileStore()
-mobile_store.add_mobile(samsung)
+mobile_store.add_mobile(oneplus)
 mobo_phone = mobile_store.mobiles
 print(mobo_phone[0].name)
+
+samsung = 'samsung galaxy s8'
+mobile_store.add_mobile(samsung)
+samsung_phone = mobile_store.mobiles
+print(samsung_phone)
